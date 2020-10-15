f77d32e951945cd4ab4f2a9277085867943398cc
Marek <marekbauer07@gmail.com>
2020-10-15 16:21:03 +0200

    Added Pattern class

--- src/main/resources/static/js/pattern.js
+++ src/main/resources/static/js/pattern.js
@@ -1,3 +1,17 @@
 class Pattern{
+    constructor(pattern){
+        this.array = pattern;
+        this.size = pattern.length;
+    }
 
+    getColor(x, y){
+        return this.array[mod(x, this.size)][mod(y, this.size)]
+    }
 }
+
+function getSingleColorPattern(color){
+    return new Pattern([[color]]);
+}
+
+const BASE_PATTERN = new Pattern([[new Color(0,0,0), new Color(255,255,255)],
+                                          [new Color(255,255,255), new Color(0,0,0)]]);
