d06a2e5c20d80c0adb6f1c94f73e9283e5c589c9
Marek <marekbauer07@gmail.com>
2020-11-15 11:29:34 +0100

    Added documentation of Player class

--- src/main/resources/static/js/player.js
+++ src/main/resources/static/js/player.js
@@ -1,6 +1,24 @@
+/**
+ * Class representing player in game
+ *
+ * @author Marek Bauer
+ */
 class Player {
-    constructor(color, posX, posY, path, movX, movY, pattern=getSingleColorPattern(color)) {
+    /**
+     * Constructor of player
+     * @param color : Color - color of player
+     * @param name : String - name of player
+     * @param posX - current position in pixels
+     * @param posY - current position in pixels
+     * @param path : [[int]] - array of positions of turns in pixels. eg [[0,0], [30, 0]] means path started in (0, 0),
+     *                         goes to (30, 0) and to current player's position
+     * @param movX - current speed of player
+     * @param movY - current speed of player
+     * @param pattern : Pattern - pattern of this player
+     */
+    constructor(color, name, posX, posY, path, movX, movY, pattern = getSingleColorPattern(color)) {
         this.color = color;
+        this.name = name;
         this.posX = posX;
         this.posY = posY;
         this.path = path;
@@ -9,6 +27,14 @@ class Player {
         this.pattern = pattern;
     }
 
+    /**
+     * Function displaying player on canvas
+     * @param ctx - canvas context
+     * @param viewX - x coordinate of camera
+     * @param viewY - y coordinate of camera
+     * @param viewW - width of camera view
+     * @param viewH - height of camera view
+     */
     draw(ctx, viewX, viewY, viewW, viewH) {
         const C = PLAYER_RADIUS;
         ctx.lineWidth = LINE_WIDTH;
@@ -30,6 +56,15 @@ class Player {
         }
     }
 
+    /**
+     * Function displaying dying player animation on canvas
+     * @param ctx - canvas context
+     * @param viewX - x coordinate of camera
+     * @param viewY - y coordinate of camera
+     * @param viewW - width of camera view
+     * @param viewH - height of camera view
+     * @param frame - frame of dying animation
+     */
     drawDying(ctx, viewX, viewY, viewW, viewH, frame) {
         const C = PLAYER_RADIUS;
         if (LINE_WIDTH > frame) {
@@ -47,7 +82,25 @@ class Player {
         }
     }
 
-    #drawPath(ctx, viewX, viewY, viewW, viewH, shift = 0, C=PLAYER_RADIUS) {
+    /**
+     * Call this function to add current speed to position
+     */
+    move() {
+        this.posX += this.movX;
+        this.posY += this.movY;
+    }
+
+    /**
+     * Function drawing path behind player
+     * @param ctx - canvas context
+     * @param viewX - x coordinate of camera
+     * @param viewY - y coordinate of camera
+     * @param viewW - width of camera view
+     * @param viewH - height of camera view
+     * @param shift - number of pixels to shift path position to right and dawn
+     * @param C - extension of field of view for drawing optimization
+     */
+    #drawPath(ctx, viewX, viewY, viewW, viewH, shift = 0, C = PLAYER_RADIUS) {
         let drawing = false;
         let prev = [this.posX, this.posY];
         for (let i = this.path.length - 1; i >= 0; i--) {
@@ -71,14 +124,21 @@ class Player {
         if (drawing) ctx.stroke();
     }
 
+    /**
+     * Function checking if is rectangle ((x1, y1), (x2, y2)) has common points with ((vX, vY), (vX + vW, vY + vH))
+     * @param x1 - coordinate of first rectangle
+     * @param y1 - coordinate of first rectangle
+     * @param x2 - coordinate of first rectangle
+     * @param y2 - coordinate of first rectangle
+     * @param vX - coordinate of second rectangle
+     * @param vY - coordinate of second rectangle
+     * @param vW - width of second rectangle
+     * @param vH - height of second rectangle
+     * @returns {boolean} - result
+     */
     static visible(x1, y1, x2, y2, vX, vY, vW, vH) {
         if (x1 > x2) x2 = [x1, x1 = x2][0];
         if (y1 > y2) y2 = [y1, y1 = y2][0];
         return !(x1 > vX + vW || x2 < vX || y1 > vY + vH || y2 < vY);
-    }
-
-    move() {
-        this.posX += this.movX;
-        this.posY += this.movY;
     }
 }
