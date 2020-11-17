371193ffa52589a1f83d63847ac4742fc8df6258
Marek <marekbauer07@gmail.com>
2020-11-17 14:57:19 +0100

    Updated buttons controls

--- src/main/resources/static/js/mainScreen.js
+++ src/main/resources/static/js/mainScreen.js
@@ -1,41 +1,11 @@
 $( document ).ready(function () {
-    $('#mainScreen').attr('visible', "True");
+    //$('#mainScreen').attr('visible', "True");
 
-    $('#loginFBtn').click(function () {
-        let div = $('#loginDiv');
-        if (div.attr('visible') === 'False'){
-            closeAllWindows();
-            $(this).attr('active', 'True');
-            div.attr('visible', 'True');
-        } else {
-            $(this).removeAttr('active');
-            div.attr('visible', "False");
-        }
-    });
+    $('#loginFBtn').click(() => btnHandler($('#loginFBtn'), $('#loginDiv')));
 
-    $('#signInFBtn').click(function () {
-        let div = $('#signInDiv');
-        if (div.attr('visible') === 'False'){
-            closeAllWindows();
-            $(this).attr('active', 'True');
-            div.attr('visible', 'True');
-        } else {
-            $(this).removeAttr('active');
-            div.attr('visible', "False");
-        }
-    });
+    $('#signInFBtn').click(() => btnHandler($('#signInFBtn'), $('#signInDiv')));
 
-    $('#configFBtn').click(function () {
-        let div = $('#configDiv');
-        if (div.attr('visible') === 'False'){
-            closeAllWindows();
-            $(this).attr('active', 'True');
-            div.attr('visible', 'True');
-        } else {
-            $(this).removeAttr('active');
-            div.attr('visible', "False");
-        }
-    });
+    $('#configFBtn').click(() => btnHandler($('#configFBtn'), $('#configDiv')));
 
     $('#logInBtn').click(function () {
         $('#mainScreen').attr('logged', 'True');
@@ -54,8 +24,7 @@ $( document ).ready(function () {
 });
 
 function closeAllWindows() {
-    $('#signInFBtn').removeAttr('active');
-    $('#loginFBtn').removeAttr('active');
+    $('.floatingBtn').each((k, v) => $(v).removeAttr('active'));
     $('#signInDiv').attr('visible', "False");
     $('#loginDiv').attr('visible', "False");
 }
@@ -67,4 +36,21 @@ function loginVerify(){
 function emailVerify(){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test($('#signInEmail').val().toLowerCase());
+}
+
+function btnHandler(btn, div){
+    if (div.attr('visible') === 'False'){
+        if (!blockingPopups()) {
+            closeAllWindows();
+            btn.attr('active', 'True');
+            div.attr('visible', 'True');
+        }
+    } else {
+        btn.removeAttr('active');
+        div.attr('visible', "False");
+    }
+}
+
+function blockingPopups() {
+    return $('.blockPopup[visible="True"]').length > 0
 }
