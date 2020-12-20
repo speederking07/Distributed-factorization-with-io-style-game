971e73e020bcfd33e1ae2df20364840efc301022
Marek <marekbauer07@gmail.com>
2020-12-20 18:47:21 +0100

    Added constants descriptions

--- src/main/resources/static/js/consts.js
+++ src/main/resources/static/js/consts.js
@@ -1,20 +1,20 @@
-const BLOCK_SIZE = 40;
-const PLAYER_RADIUS = 26;
-const DYING_FRAMES = 30;
-const LINE_WIDTH = 20;
-const ROTATION_FRAMES = 16;
-const MAX_ROTATION_DELAY = 20;
-const BACKGROUND_SHIFT = 0.25;
-const FRAMES_PER_SECONDS = 30;
-const SPEED = 2;
-const TURNS_PER_SECONDS = 4;
-const NUMBER_OF_STEPS = BLOCK_SIZE/SPEED;
-const MOVE_WINDOW = Math.floor(2*NUMBER_OF_STEPS/3);
+const BLOCK_SIZE = 40; //Size of single tile on board
+const PLAYER_RADIUS = 26; //Radius of player's checker
+const DYING_FRAMES = 30; //Length of dying animations in frames
+const LINE_WIDTH = 20; //Width of line of path behind players
+const ROTATION_FRAMES = 16; //Length of tile animation in frames
+const MAX_ROTATION_DELAY = 20; //Maximal number of frames to wait to begin change tile animation
+const BACKGROUND_SHIFT = 0.25; //Number of pixes to move background per one pixel of moved foreground
+const FRAMES_PER_SECONDS = 30; //Caped FPS in game
+const SPEED = 2; //Number of pixels to move on one step
+const TURNS_PER_SECONDS = 4; //Number of tiles to move to in single seconds
+const NUMBER_OF_STEPS = BLOCK_SIZE / SPEED; //Number of steps to move from one tile to another
+const MOVE_WINDOW = Math.floor(2 * NUMBER_OF_STEPS / 3); //Number of step after witch player can not change direction
 
 function mod(a, n) {
     return ((a % n) + n) % n
 }
 
 function arrayRemove(arr, value) {
-    return arr.filter(ele => ele !== value );
+    return arr.filter(ele => ele !== value);
 }
