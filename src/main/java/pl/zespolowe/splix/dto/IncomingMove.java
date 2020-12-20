4839ac567e4b6ade70b57b536658167102dcc6e1
Masioki <49079199+Masioki@users.noreply.github.com>
2020-12-20 17:06:56 +0100

    Import fix

--- src/main/java/pl/zespolowe/splix/dto/IncomingMove.java
+++ src/main/java/pl/zespolowe/splix/dto/IncomingMove.java
@@ -2,7 +2,8 @@ package pl.zespolowe.splix.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.domain.Sort.Direction;
+import pl.zespolowe.splix.domain.game.Direction;
+
 
 @Getter
 @Setter
