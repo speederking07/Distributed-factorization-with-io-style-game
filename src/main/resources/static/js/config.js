b6450b8fe7e7676cb53b856ba1d3e736c67fdbbb
Marek <marekbauer07@gmail.com>
2020-12-20 18:38:10 +0100

    Added documentation

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -1,21 +1,27 @@
-$( document ).ready(function () {
+/**
+ * Code of handling changing settings
+ *
+ * @author Marek Bauer
+ */
+
+$(document).ready(function () {
     $('#colorInput').change(function () {
         let color = Color.fromHex($(this).val());
         let label = $('#colorLabel');
         label.css('background-color', color.get());
-        label.css('box-shadow', color.darken().get()+" 2px 2px")
+        label.css('box-shadow', color.darken().get() + " 2px 2px")
     });
 
-    $('#colorLabel').click(event =>{
+    $('#colorLabel').click(event => {
         event.preventDefault();
         openColorPicker($('#colorInput'))
     });
 
-    $('#cloneColorBtn').click(()=>{
+    $('#cloneColorBtn').click(() => {
         setAllPatternColor($('#colorInput').val())
     });
 
-    $('#saveConfig').click(function (){
+    $('#saveConfig').click(function () {
         $('#configDiv').attr('visible', 'False');
         $('#configFBtn').removeAttr('active');
         $('.colorPickerDiv').remove();
@@ -28,14 +34,14 @@ $( document ).ready(function () {
         updateSettings(settings);
     });
 
-    $('#quitConfig').click(function (){
+    $('#quitConfig').click(function () {
         $('#configDiv').attr('visible', 'False');
         $('#configFBtn').removeAttr('active');
         $('.colorPickerDiv').remove();
         refreshSettings();
     });
 
-    $('#changeGraphicalSettings').click(function (){
+    $('#changeGraphicalSettings').click(function () {
         updateDisplay();
 
         $('#settingDiv').attr('visible', 'False');
@@ -53,6 +59,9 @@ $( document ).ready(function () {
     generatePattern();
 });
 
+/**
+ * Function downloading user setting from server and updating then
+ */
 function refreshSettings() {
     $.get({
         url: "/user/settings",
@@ -60,17 +69,17 @@ function refreshSettings() {
         success: function (data) {
             pattern = Pattern.FromJSON(data.colorsInCSV);
             color = Color.formJson(data.colorsInCSV);
-            if (data.namesAbove){
+            if (data.namesAbove) {
                 $('#namesBox').attr("checked", "");
             } else {
                 $('#namesBox').removeAttr("checked");
             }
-            if (data.boardAnimation){
+            if (data.boardAnimation) {
                 $('#boardAnimationsBox').attr("checked", "");
             } else {
                 $('#boardAnimationsBox').removeAttr("checked");
             }
-            if (data.dyingAnimation){
+            if (data.dyingAnimation) {
                 $('#killingAnimationsBox').attr("checked", "");
             } else {
                 $('#killingAnimationsBox').removeAttr("checked");
@@ -81,21 +90,26 @@ function refreshSettings() {
     });
 }
 
-function updateDisplay(){
+/**
+ * Function setting new display preferences by checkin form
+ */
+function updateDisplay() {
     $("#colorInput").val(color.toHex());
     $("#colorInput").change();
     let config = [$('#namesBox').is(':checked'),
         $('#boardAnimationsBox').is(':checked'),
         $('#killingAnimationsBox').is(':checked')];
     BoardView.prototype.getGraphicalSettings = () => config;
-    if(demo instanceof Demo){
+    if (demo instanceof Demo) {
         demo.view.draw = demo.view.generateDrawFunction(config[0], config[1], config[2]);
     }
 }
 
+/**
+ * Function uploading current setting to server
+ * @param settings - current settings abzect
+ */
 function updateSettings(settings) {
-
-    // tak ma wygladac obiekt do tego requesta, odpowiednik UserSettingsDTO
     //settings = JSON.stringify({namesAbove:true, boardAnimation:true, dyingAnimation:true, colorsInCSV:"#010000;#000000;#000000;#000000;#000000;#000000;\n#000000;#000000;#000000;#000000;#000000;#000000;\n#000000;#000000;#000000;#000000;#000000;#000000;\n#000000;#000000;#000000;#000000;#000000;#000000;\n#000000;#000000;#000000;#000000;#000000;#000000;\n#000000;#000000;#000000;#000000;#000000;#000000;\n"})
     const s = settings;
     const toSent = JSON.stringify(settings);
@@ -111,35 +125,38 @@ function updateSettings(settings) {
     });
 }
 
+/**
+ * Function generating changing pattern form
+ */
 function generatePattern() {
     let patDiv = '';
-    const a = [3,4,5,0,1,2,3,4,5,0,1,2];
-    for(let x = 0; x < 3; x++){
+    const a = [3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 1, 2];
+    for (let x = 0; x < 3; x++) {
         patDiv += "<div class='extended'>";
-        for(let y = 0; y < 12; y++){
-            patDiv += '<label class="patternBox" for="p'+a[x]+a[y]+'" col="'+a[x]+'" row="'+a[y]+'"></label>'
+        for (let y = 0; y < 12; y++) {
+            patDiv += '<label class="patternBox" for="p' + a[x] + a[y] + '" col="' + a[x] + '" row="' + a[y] + '"></label>'
         }
         patDiv += "</div>";
     }
-    for(let x = 3; x < 9; x++){
+    for (let x = 3; x < 9; x++) {
         patDiv += "<div>";
-        for(let y = 0; y < 12; y++){
-            if(y === 0 || y===9) patDiv += "<span class='extended'>";
-            patDiv += '<label class="patternBox" for="p'+a[x]+a[y]+'" col="'+a[x]+'" row="'+a[y]+'"></label>';
-            if(y === 2 || y===11) patDiv += "</span>";
+        for (let y = 0; y < 12; y++) {
+            if (y === 0 || y === 9) patDiv += "<span class='extended'>";
+            patDiv += '<label class="patternBox" for="p' + a[x] + a[y] + '" col="' + a[x] + '" row="' + a[y] + '"></label>';
+            if (y === 2 || y === 11) patDiv += "</span>";
         }
         patDiv += "</div>";
     }
-    for(let x = 9; x < 12; x++){
+    for (let x = 9; x < 12; x++) {
         patDiv += "<div class='extended'>";
-        for(let y = 0; y < 12; y++){
-            patDiv += '<label class="patternBox" for="p'+a[x]+a[y]+'" col="'+a[x]+'" row="'+a[y]+'"></label>';
+        for (let y = 0; y < 12; y++) {
+            patDiv += '<label class="patternBox" for="p' + a[x] + a[y] + '" col="' + a[x] + '" row="' + a[y] + '"></label>';
         }
         patDiv += "</div>";
     }
-    for(let x = 0; x < 6; x++){
-        for(let y = 0; y < 6; y++){
-            patDiv += '<input type="color" id="p'+x+y+'" col="'+x+'" row="'+y+'">'
+    for (let x = 0; x < 6; x++) {
+        for (let y = 0; y < 6; y++) {
+            patDiv += '<input type="color" id="p' + x + y + '" col="' + x + '" row="' + y + '">'
         }
     }
     $('#choosePatternDiv').html(patDiv);
@@ -150,37 +167,51 @@ function generatePattern() {
         let color = Color.fromHex($(this).val());
         let c1 = color.get();
         let c2 = color.lighten().get();
-        $('label[row="'+row+'"][col="'+col+'"]').css('background-color', c1);
-        $('.extended label[row="'+row+'"][col="'+col+'"]').css('background-color', c2);
+        $('label[row="' + row + '"][col="' + col + '"]').css('background-color', c1);
+        $('.extended label[row="' + row + '"][col="' + col + '"]').css('background-color', c2);
     });
 
     setAllPatternColor('#ff0000');
 
-    $('#choosePatternDiv label').click(event =>{
+    $('#choosePatternDiv label').click(event => {
         event.preventDefault();
         openColorPicker($('#' + $(event.target).attr('for')));
     });
 }
 
+/**
+ * Setting pattern choosing form to one color
+ * @param color{Color} - color to set to
+ */
 function setAllPatternColor(color) {
-    $('#choosePatternDiv input[type=color]').each((_, target) =>{
+    $('#choosePatternDiv input[type=color]').each((_, target) => {
         $(target).val(color);
         $(target).change();
     });
 }
 
+/**
+ * Setting pattern choosing form to pattern
+ * @param pattern{Pattern} - pattern to set to
+ */
 function loadPattern(pattern) {
-    for (const x of Array(6).keys()){
+    for (const x of Array(6).keys()) {
         for (const y of Array(6).keys()) {
-            const handler = $('#p'+x+y);
-            handler.val(pattern.getColor(x,y).toHex());
+            const handler = $('#p' + x + y);
+            handler.val(pattern.getColor(x, y).toHex());
             handler.change()
         }
     }
 }
 
-function openColorPicker(input){
-    if($('.colorPickerDiv').length === 0) {
+/**
+ * Function opening color picker form if it is possible
+ *
+ * @param input - handler of input to save data to
+ * @returns {boolean} - if was onend
+ */
+function openColorPicker(input) {
+    if ($('.colorPickerDiv').length === 0) {
         $('body').append("<div class='colorPickerDiv floatingDiv'>" +
             "<h1>Choose color</h1>" +
             "<div class=\"wheel\" id=\"colorWheel\"></div>" +
@@ -205,18 +236,21 @@ function openColorPicker(input){
             $('.colorPickerDiv').remove();
         });
         return true;
-    }
-    else{
+    } else {
         return false;
     }
 }
 
+/**
+ * Converting current pattern from pattern selection form to pattern object
+ * @returns {Pattern}
+ */
 function currentConfigToPattern() {
     let pattern = [];
-    for(let x = 0; x < 6; x++){
+    for (let x = 0; x < 6; x++) {
         pattern[x] = [];
-        for(let y = 0; y < 6; y++){
-            pattern[x][y] = Color.fromHex($('#p'+x+y).val());
+        for (let y = 0; y < 6; y++) {
+            pattern[x][y] = Color.fromHex($('#p' + x + y).val());
         }
     }
     return new Pattern(pattern)
