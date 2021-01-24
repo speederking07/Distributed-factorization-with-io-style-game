3e1b1fbbb1d6abd3d85edd33f6fb20a741039170
KalinaMichal <michal230915@gmail.com>
2021-01-24 16:03:13 +0100

    Board test first part

---
+++ src/test/java/pl/zespolowe/splix/domain/game/BoardTest.java
@@ -0,0 +1,22 @@
+package pl.zespolowe.splix.domain.game;
+
+/***
+ *
+ * @author KalinaMichal
+ * Tests for Board class
+ *
+ */
+public class BoardTest {
+    @org.junit.Test
+    public void startGame(){
+
+        //given
+        Board sut = new Board(20,20);
+
+        //when
+
+
+        //then
+
+    }
+}
